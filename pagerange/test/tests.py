3ddd3bfca9e84a3b69d9179edfbfee30e05afd9f
Zach <taylorzr@gmail.com>
2014-05-23 14:21:37 -0500

    Added readme & tests. Cleaned up PageRange class.

---
+++ pagerange/test/tests.py
@@ -0,0 +1,11 @@
+from pagerange import PageRange
+
+pr = PageRange([1,2,3,5,10])
+pr.add_page(6)
+pr.remove_page(5)
+print pr, "->", pr.pages
+
+pr = PageRange("1-3,5,10")
+pr.add_page(6)
+pr.remove_page(5)
+print pr, "->", pr.pages
