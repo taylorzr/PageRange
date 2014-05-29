364c9d1544b103d6e4cde38788c6d89c4f873713
Zach Taylor <taylorzr@gmail.com>
2014-05-29 00:14:01 -0500

    Added license and renamed test file.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.2',
     packages=[''],
     url='http://github.com/taylorzr/pagerange',
-    license='',
+    license='MIT',
     author='Zach',
     author_email='taylorzr@gmail.com',
     description='Pagerange is a simple module that provides a PageRange object typically used in printing.'
